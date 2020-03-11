3f5bacdbfcff03e7feec13a7c3b4a84a24147736
Watsapon 'Ty' Kampong <Watsapon.k@kaidee.com>
2020-03-11 12:29:52 +0700

    feat: add damage dealer script

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] int health = 200;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        DamageDealer damageDealer = collision.gameObject.GetComponent<DamageDealer>();
+        if(damageDealer != null)
+        {
+            health -= damageDealer.GetDamage();
+            damageDealer.Hit();
+            if(health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
