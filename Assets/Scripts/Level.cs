110230647558eabe0e91a53541bc675db1c5d0b0
Watsapon 'Ty' Kampong <Watsapon.k@kaidee.com>
2020-03-24 13:44:49 +0700

    feat: create start menu and game over scene

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene("GameStart");
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
