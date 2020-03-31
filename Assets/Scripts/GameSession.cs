cf4040a424f3e43e0623abd1abbb0df274054896
Watsapon 'Ty' Kampong <Watsapon.k@kaidee.com>
2020-03-31 12:46:19 +0700

    feat: add GameSession into all scene

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSession : MonoBehaviour
+{
+    private int currentScore = 0;
+
+    void Awake()
+    {
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void AddScore(int score)
+    {
+        currentScore += score;
+    }
+
+    public int GetScore()
+    {
+        return currentScore;
+    }
+
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+
+}
